0bf1332ca1bad6d08b99587208ec21b9cb307821
rcpsilva <rcpsilva@gmail.com>
2022-09-21 10:03:02 -0300

    [chore] top down and bottom up proofs

--- 2022-1/Logical Agents/logical_agents.py
+++ 2022-1/Logical Agents/logical_agents.py
@@ -1,111 +1,67 @@
-import copy as cp
+def botton_up(kb):
+    C = []
 
-def prove_TD(kb,G):
-    print(G)
-    if not G:
+    if 'askables' in kb:
+        for a in kb['askables']:
+            if ask(a):
+                C.append(a)
+
+    new_consequence = True
+
+    while new_consequence:
+        new_consequence = False 
+
+        for head in kb['rules']:
+            if head not in C: # Very innefient
+                for body in kb['rules'][head]:
+                    if not set(body).difference(set(C)): # Very innefient
+                        C.append(head)
+                        new_consequence = True
+
+    return C
+
+def top_down(querry,kb):
+
+    print(querry)
+
+    if not querry:
         return 'yes.'
     else:
-        a = G.pop(0)
+        a = querry.pop(0)
         
         if a in kb['askables']:
-            if ask(a):
+            a_val = ask(a)
+            kb['askables'].remove(a)
+            if a_val:
                 kb['rules'][a] = [[]]
-                kb['askables'].remove(a)
 
-        if a in kb['rules']:
-            bodies = kb['rules'][a]
+        if a not in kb['rules']:
+            return f'Cannot prove {querry}'
+        
+        bodies = kb['rules'][a]
+        
+        for b in bodies:
+            res = top_down(b + querry,kb)
+            if res == 'yes.':
+                return res
+        
+        return f'Cannot prove {querry}.'
 
-            for body in bodies:
-                if prove_TD(kb,body + G) == 'yes.':
-                    return 'yes.' 
-        return f'Could not prove for {a}'
-
-def prove_BU(kb):
-
-    nkb = cp.deepcopy(kb)
-
-    for askable in kb['askables']:
-        if ask(askable):
-            nkb['rules'][askable] = [[]]
-    c = []
-    added_atom = True
-
-    while added_atom:
-        added_atom = False
-
-        for h in nkb['rules']:
-            if h not in c:
-                bodies = nkb['rules'][h]
-                for body in bodies:
-                    if set(body).issubset(set(c)):
-                        c.append(h)
-                        added_atom = True
-                        break
-    return c            
 
 def ask(askable):
     ans = input(f'Is {askable} true?')
-    return True if ans in ['sim','yes','y','s'] else False
-
-def explain(observations, kb, explanation = set()):
-    if observations:
-        a = observations[0]
-
-        if a in kb['assumables']:
-            return explain(observations[1:],kb,explanation|{a})
-        else:
-            bodies = kb['rules'][a]
-            explanations = []
-
-            for body in bodies:
-                explanations += explain(body + observations[1:],kb,explanation)
-
-            return explanations
-    return [explanation]
-
-def printkb(kb):
-
-    rules = kb['rules']
-
-    print('Rules:')
-    for r in rules:
-        bodies = rules[r]
-        for b in bodies:
-            if b:
-                print('{} <- {}'.format(r,' & '.join(b)))
-            else:
-                print('{}.'.format(r))
-    
-    
-    if 'askables' in kb:
-        print('Askables:')
-        print(kb['askables'])
-
-    if 'assumables' in kb:
-        print('Assumables:')
-        print(kb['assumables'])
-
+    return True if ans.lower() in ['sim','s','yes','y'] else False
 
 if __name__ == "__main__":
     
-    kb1 = {  'rules':{   'a':[['b','c']],
-                        'b':[['d'],['c']],
-                        'e':[[]],
-                        'd':[['h']],
-                        'g':[['a','b','c']],
-                        'f':[['h','b']]},
+    kb = {'rules':{'a':[['b','c']],
+                   'b':[['d'],['c']],
+                   'd':[['h']],
+                   'g':[['a','b','c']],
+                   'f':[['h','b']]},
             'askables':['c','e']}
 
-    kb2 = {  'rules':{   'bronchitis':[['influenza'],['smokes']],
-                        'coughing':[['bronchitis']],
-                        'wheezing':[['bronchitis']],
-                        'fever':[['influenza'],['infection']],
-                        'sore_throat':[['influenza']],
-                        'False':[['smokes','non-smoker']]
-                        },
-            'assumables':['smokes','influenza','infection'] }
 
+    print(top_down(['g'],kb))
 
-    #print(explain(['False'],kb))
-
-    printkb(kb1)
+    print(botton_up(kb))
