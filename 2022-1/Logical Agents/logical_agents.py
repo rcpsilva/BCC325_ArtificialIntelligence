360be698ee74ea8c121552f64136bc7d8ef4dd6e
rcpsilva <rcpsilva@gmail.com>
2022-10-03 09:18:47 -0300

    [chrore] abduction and minimal explanations

--- 2022-1/Logical Agents/logical_agents.py
+++ 2022-1/Logical Agents/logical_agents.py
@@ -52,6 +52,38 @@ def ask(askable):
     ans = input(f'Is {askable} true?')
     return True if ans.lower() in ['sim','s','yes','y'] else False
 
+def explain(observations,kb,explanation=set()):
+
+    if observations:
+        o = observations[0]
+
+        if o in kb['assumables']:
+            return explain(observations[1:],kb,explanation|{o})
+        else:
+            bodies = kb['rules'][o]
+            explanations = []
+
+            for body in bodies:
+                explanations += explain(body + observations[1:],kb,explanation)
+
+            return explanations
+
+    return [explanation]
+
+
+def get_minimal_explanations(explanations):
+
+    not_minimal = []
+
+    for i in range(len(explanations)):
+        for j in range(len(explanations)):
+            if explanations[i].issubset(explanations[j]) and i != j:
+                not_minimal.append(j)
+
+    return [explanations[i] for i in range(len(explanations)) if i not in not_minimal]
+
+
+
 if __name__ == "__main__":
     
     kb = {'rules':{'a':[['b','c']],
@@ -62,9 +94,9 @@ if __name__ == "__main__":
             'askables':['c','e']}
 
 
-    print(top_down(['g'],kb))
+    #print(top_down(['g'],kb))
 
-    print(botton_up(kb))
+    #print(botton_up(kb))
 
     kb2 = {  'rules':{  'bronchitis':[['influenza'],['smokes']],
                         'coughing':[['bronchitis']],
@@ -73,4 +105,10 @@ if __name__ == "__main__":
                         'sore_throat':[['influenza']],
                         'False':[['smokes','non-smoker']]
                         },
-            'assumables':['smokes','influenza','infection'] }
+            'assumables':['smokes','influenza','infection'] }+
+
+    explanations = explain(['wheezing','fever'],kb2)
+
+    print(explanations)
+
+    print(get_minimal_explanations(explanations))
