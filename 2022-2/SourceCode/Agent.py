afa7966ccfba0ff09bad69e7607cdc48abd2d8c5
rcpsilva <rcpsilva@gmail.com>
2022-12-14 16:47:42 -0300

    [feat] Implemented the first agent

---
+++ 2022-2/SourceCode/Agent.py
@@ -0,0 +1,31 @@
+
+class Agent():
+    def __init__(self, env):
+        self.env = env
+        self.percepts = env.initial_percepts()
+        self.F = [[self.percepts['position']]]
+
+    def act(self):
+        while self.F:
+            path = self.F.pop(0)
+
+            action = {'position':path[-1]}
+            self.percepts = self.env.change_state(action)
+
+            if path[-1] == self.percepts['goal']:
+                return path
+            else:
+                for n in self.percepts['neighbors']:
+                    if n not in path:
+                        self.F.append(path + [n])
+
+        print('Impossible!')
+        return []
+
+
+
+
+
+
+
+
