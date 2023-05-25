864605de57e92aa43b66fa281c593ad5fe83641e
rcpsilva <rcpsilva@gmail.com>
2023-05-24 23:06:02 -0300

    added greedy agent

--- 2023-1/Source Code/agents.py
+++ 2023-1/Source Code/agents.py
@@ -22,31 +22,14 @@ class AgentDFS:
             action = {'mover_para':path[-1]}
 
             ################ VIS ###########
-            ## Plotar caminho
-            caminho = path
-
-            plt.axes().invert_yaxis()
-            plt.pcolormesh(self.ambiente.map)
-            for i in range(len(caminho)-1):
-                plt.plot([caminho[i][1]+0.5,caminho[i+1][1]+0.5],[caminho[i][0]+0.5,caminho[i+1][0]+0.5],'-rs')
-            plt.draw()
-            plt.pause(0.1)
-            plt.clf()
+            plotar_caminho(self.ambiente,path,0.001)
             ################################
 
             self.percepcoes = self.ambiente.muda_estado(action) 
 
             if (self.percepcoes['posicao'] == self.percepcoes['saida']).all():
                 ################ VIS ###########
-                ## Plotar caminho
-                plt.axes().invert_yaxis()
-                plt.pcolormesh(self.ambiente.map)
-                caminho = path
-                for i in range(len(caminho)-1):
-                    plt.plot([caminho[i][1]+0.5,caminho[i+1][1]+0.5],[caminho[i][0]+0.5,caminho[i+1][0]+0.5],'-rs')
-                plt.draw()
-                plt.pause(3)
-                plt.clf()
+                self.plotar_caminho(self.ambiente,path,3)
                 ################################
                 return path
             else:
@@ -59,7 +42,66 @@ class AgentDFS:
                     if not forma_ciclo:
                         self.F.append(path + [vi])
         
+class AgentGreedy:
+    def __init__(self, ambiente) -> None:
+        self.ambiente = ambiente
+        self.percepcoes = ambiente.percepcoes_iniciais()
+        self.F = [[self.percepcoes['posicao']]]
+        self.C = []
+        self.H = [heuristica(self.percepcoes['posicao'],self.percepcoes['saida'])]
+        self.C_H = []
 
+    def act(self):
+
+        # Executar ação
+        # Coletar novas percepções
+
+        plt.ion()
+        while self.F:
+            
+            posicao_min_h_na_fronteira = np.argmin(self.H)
+            
+            path = self.F.pop(posicao_min_h_na_fronteira)
+            self.H.pop(posicao_min_h_na_fronteira) 
+            
+            action = {'mover_para':path[-1]}
+
+            ################ VIS ###########
+            plotar_caminho(self.ambiente,path,0.001)
+            ################################
+
+            self.percepcoes = self.ambiente.muda_estado(action) 
+
+            if (self.percepcoes['posicao'] == self.percepcoes['saida']).all():
+                ################ VIS ###########
+                self.plotar_caminho(self.ambiente,path,4)
+                ################################
+                return path
+            else:
+                for vi in self.percepcoes['vizinhos']:
+                    forma_ciclo = False
+                    for no in path:
+                        if (no == vi).all():
+                            forma_ciclo = True
+
+                    if not forma_ciclo:
+                        self.F.append(path + [vi])
+                        self.H.append(heuristica(vi,self.percepcoes['saida']))
+
+
+def heuristica(no, objetivo):
+    manhattan_distance = np.sum(np.abs(no-objetivo))
+    return manhattan_distance
+
+
+def plotar_caminho(ambiente, caminho, tempo):
+    plt.axes().invert_yaxis()
+    plt.pcolormesh(ambiente.map)
+    for i in range(len(caminho)-1):
+        plt.plot([caminho[i][1]+0.5,caminho[i+1][1]+0.5],[caminho[i][0]+0.5,caminho[i+1][0]+0.5],'-rs')
+    plt.draw()
+    plt.pause(tempo)
+    plt.clf()
 
 
            
--- 2023-1/Source Code/main.py
+++ 2023-1/Source Code/main.py
@@ -1,11 +1,11 @@
 from Labirinto import Labirinto
-from agents import AgentDFS
+from agents import AgentDFS, AgentGreedy
 
-nlin = 7
-ncol = 7
+nlin = 13
+ncol = 13
 
-l1 = Labirinto(nlin,ncol,0.25,[0,0],[nlin-1,ncol-1]) 
+l1 = Labirinto(nlin,ncol,0.3,[0,0],[nlin-1,ncol-1]) 
 
-ag = AgentDFS(l1)
+ag = AgentGreedy(l1)
 caminho = ag.act()
 print(caminho)
