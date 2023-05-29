426d5b691447ef4b3f7d75541c60a1aec19424ef
rcpsilva <rcpsilva@gmail.com>
2023-05-29 14:45:31 -0300

    Added problem Set 4 - CSPs

--- 2023-1/Source Code/main.py
+++ 2023-1/Source Code/main.py
@@ -4,7 +4,7 @@ from agents import AgentDFS, AgentGreedy
 nlin = 13
 ncol = 13
 
-l1 = Labirinto(nlin,ncol,0.3,[0,0],[nlin-1,ncol-1]) 
+l1 = Labirinto(nlin,ncol,0.25,[0,0],[nlin-1,ncol-1]) 
 
 ag = AgentGreedy(l1)
 caminho = ag.act()
