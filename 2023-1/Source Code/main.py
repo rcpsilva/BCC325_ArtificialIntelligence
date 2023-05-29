e94a3609d47f167fa8b24845474a12e90a310041
rcpsilva <rcpsilva@gmail.com>
2023-05-29 15:38:32 -0300

    Update main.py

--- 2023-1/Source Code/main.py
+++ 2023-1/Source Code/main.py
@@ -1,8 +1,8 @@
 from Labirinto import Labirinto
 from agents import AgentDFS, AgentGreedy
 
-nlin = 13
-ncol = 13
+nlin = 12
+ncol = 12
 
 l1 = Labirinto(nlin,ncol,0.25,[0,0],[nlin-1,ncol-1]) 
 ag = AgentGreedy(l1)
