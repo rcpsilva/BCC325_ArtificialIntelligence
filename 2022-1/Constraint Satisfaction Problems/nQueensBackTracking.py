52ce152ff6577636568ff5eb1168c3816f52d412
rcpsilva <rcpsilva@gmail.com>
2023-05-30 21:55:38 -0300

    added chessboard to the n-queen problem

--- 2022-1/Constraint Satisfaction Problems/nQueensBackTracking.py
+++ 2022-1/Constraint Satisfaction Problems/nQueensBackTracking.py
@@ -1,3 +1,46 @@
+import pygame
+import sys
+
+def draw_chessboard(queen_positions):
+
+    queen_positions = [(i,queen_positions[i]) for i in range(len(queen_positions))]
+
+    # Set up colors
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+
+    # Set up the window size
+    WIDTH = 480
+    HEIGHT = 480
+
+    # Calculate cell width and height
+    CELL_SIZE = WIDTH // 8
+
+
+    # Initialize Pygame
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption('Chessboard')
+
+    # Main game loop
+    while True:
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # Draw the chessboard
+        for row in range(8):
+            for col in range(8):
+                color = WHITE if (row + col) % 2 == 0 else BLACK
+                pygame.draw.rect(screen, color, (col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+                if (row, col) in queen_positions:
+                    pygame.draw.circle(screen, RED, (col * CELL_SIZE + CELL_SIZE // 2, row * CELL_SIZE + CELL_SIZE // 2), CELL_SIZE // 3)
+
+        # Update the display
+        pygame.display.flip()
 
 def r0e7(sol,val):
     if len(sol) > 0:
@@ -30,13 +73,27 @@ def check_constraints(sol,val,constraints):
 def search(domain, constraints, sol=[]):
 
     if len(sol) == len(domain):
-        print(sol)
+        return sol
     else:
         for d in domain[len(sol)]:
             if check_constraints(sol, d, constraints):
                 sol.append(d)
-                search(domain,constraints,sol)
+                result = search(domain,constraints,sol)
+                if result:
+                    return result
                 sol.pop(-1)
+
+# def draw_chessboard(queen_positions):
+#     n = len(queen_positions)
+#     board = [['-'] * n for _ in range(n)]
+
+#     for row, col in enumerate(queen_positions):
+#         board[row][col] = 'Q'
+
+#     for row in range(n):
+#         for col in range(n):
+#             print(board[row][col], end=' ')
+#         print()
 
 if __name__ == '__main__':
     
@@ -45,4 +102,6 @@ if __name__ == '__main__':
     constraints = [different_column, different_diagonal,r0e7]
     
     print(domain)
-    search(domain, constraints)
+    result = search(domain, constraints)
+    print(result)
+    draw_chessboard(result)
