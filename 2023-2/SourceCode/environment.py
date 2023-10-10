c28ceaf28bd620ce7b40f51dc790f1183b36b260
rcpsilva <rcpsilva@gmail.com>
2023-10-10 13:56:07 -0300

    Added visualization capabilities

--- 2023-2/SourceCode/environment.py
+++ 2023-2/SourceCode/environment.py
@@ -1,8 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Maze():
 
-    def __init__(self,nrow,ncol,start,exit,pobs=.3):
+    def __init__(self,nrow,ncol,start,exit,pobs=.3,pause=1):
 
         self.map = np.zeros((nrow,ncol))
         self.start = np.array(start)
@@ -15,12 +16,24 @@ class Maze():
                 if np.random.rand() < pobs and ([i,j]!=self.start).any() and ([i,j]!=self.exit).any():
                     self.map[i][j] = 1
 
+        self.map[start[0]][start[1]] = 0.8 
+        self.map[exit[0]][exit[1]] = 0.3
+
+        ################## visualization ###################
+        self.pause = pause
+        plt.ion()
+        self.vis_map()
+        plt.draw()
+        plt.pause(pause)
+        plt.clf()
+        ###################################################
                    
     def initial_percepts(self):
 
         return {'pos':self.start,
                 'exit':self.exit,
-                'neighbors':self.get_neighbors(self.start)}
+                'neighbors':self.get_neighbors(self.start),
+                'path':[]}
     
     def get_neighbors(self,pos):
 
@@ -32,9 +45,33 @@ class Maze():
     
     def state_transition(self,action):
 
+        ################## visualization ###################
+        plt.ion()
+        self.plot_path(action['path'],self.pause)
+        ####################################################
+
         return {'pos': action['move_to'],
                 'exit':self.exit,
-                'neighbors':self.get_neighbors(action['move_to'])}
+                'neighbors':self.get_neighbors(action['move_to']),
+                'path':action['path']}
+    
+    # Visualization functions
+    def plot_path(self, path, pause_time):
+        plt.axes().invert_yaxis()
+        plt.pcolormesh(self.map)
+        for i in range(len(path)-1):
+            plt.plot([path[i][1]+0.5,path[i+1][1]+0.5],[path[i][0]+0.5,path[i+1][0]+0.5],'-rs')
+        plt.draw()
+        plt.pause(pause_time)
+        plt.clf()
+
+    def vis_map(self):
+        plt.axes().invert_yaxis()
+        plt.pcolormesh(self.map)
+        plt.plot(self.start[1]+0.5, self.start[0]+0.5,'rs')
+        plt.show()
+
+
 
 if __name__ == '__main__':
 
@@ -44,9 +81,13 @@ if __name__ == '__main__':
 
     print(env.map)
 
-    print(env.initial_percepts())
-
-    print(env.get_neighbors(np.array([1,2])))
-    print(env.get_neighbors(np.array([1,1])))
-    print(env.get_neighbors(np.array([3,3])))
-    print(env.get_neighbors(np.array([2,2])))
+    actions = []
+    actions.append({'move_to':[0,0],
+              'path':[[0,0],[0,1],[0,2],[0,3],[1,3],[2,3]]})
+    actions.append({'move_to':[0,0],
+              'path':[[0,0],[1,1],[2,2],[3,3]]})
+    actions.append({'move_to':[0,0],
+              'path':[[0,0],[1,0],[2,0],[3,0]]})
+    
+    for a in actions:
+        env.state_transition(a)
