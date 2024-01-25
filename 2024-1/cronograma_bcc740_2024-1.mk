1647bdc9a9fa106c875b64f906fbb313ae06ea29
rcpsilva <rcpsilva@gmail.com>
2024-01-25 15:02:46 -0300

    Update cronograma_bcc740_2024-1.mk

--- 2024-1/cronograma_bcc740_2024-1.mk
+++ 2024-1/cronograma_bcc740_2024-1.mk
@@ -1,21 +1,39 @@
 ## Cronograma
 
-| Semana             | Conteúdo                                     |
-| ------------------ | -------------------------------------------- |
-| 25/03/2024 à 27/03/2024 | O que é Inteligência Artificial?         |
-| 01/04/2024 à 03/04/2024 | O que é Inteligência Artificial?         |
-| 08/04/2024 à 10/04/2024 | Busca em espaço de estados               |
-| 15/04/2024 à 17/04/2024 | Busca em espaço de estados               |
-| 22/04/2024 à 24/04/2024 | Problemas de statisfação de restrições   |
-| 29/04/2024              | Problemas de statisfação de restrições   |
-| 06/05/2024 à 08/05/2024 | Prova 1 (06/05) e Correção               |
-| 13/05/2024 à 15/05/2024 | Aprendizado de máquina                   |
-| 20/05/2024 à 22/05/2024 | Aprendizado de máquina                   |
-| 27/05/2024 à 29/05/2024 | Aprendizado de máquina                   |
-| 03/06/2024 à 05/06/2024 | Aprendizado de máquina                   |
-| 10/06/2024 à 12/06/2024 | Prova 2 (10/06) e Correção               |
-| 17/06/2024 à 19/06/2024 | Representação do conhecimento e inferência|
-| 24/06/2024 à 26/06/2024 | Representação do conhecimento e inferência|
-| 01/07/2024 à 03/07/2024 | Representação do conhecimento e inferência|
-| 10/07/2024 e 15/07/2024 | Prova 3 (10/07) e Correção                |
-| 22/07/2024              | Exame Especial                            |
+| Semana                  | Conteúdo                                     |
+| ----------------------- | -------------------------------------------- |
+| 25/03/2024 à 27/03/2024 | O que é Inteligência Artificial?             |
+| 01/04/2024 à 03/04/2024 | O que é Inteligência Artificial?             |
+| 08/04/2024 à 10/04/2024 | Busca em espaço de estados                   |
+| 15/04/2024 à 17/04/2024 | Busca em espaço de estados                   |
+| 22/04/2024 à 24/04/2024 | Problemas de statisfação de restrições       |
+| 29/04/2024              | Problemas de statisfação de restrições       |
+| 06/05/2024 à 08/05/2024 | Prova 1 (06/05) e Correção                   |
+| 13/05/2024 à 15/05/2024 | Aprendizado de máquina                       |
+| 20/05/2024 à 22/05/2024 | Aprendizado de máquina                       |
+| 27/05/2024 à 29/05/2024 | Aprendizado de máquina                       |
+| 03/06/2024 à 05/06/2024 | Aprendizado de máquina                       |
+| 10/06/2024 à 12/06/2024 | Prova 2 (10/06) e Correção                   |
+| 17/06/2024 à 19/06/2024 | Representação do conhecimento e inferência   |
+| 24/06/2024 à 26/06/2024 | Representação do conhecimento e inferência   |
+| 01/07/2024 à 03/07/2024 | Representação do conhecimento e inferência   |
+| 10/07/2024 e 15/07/2024 | Prova 3 (10/07) e Correção                   |
+| 22/07/2024              | Exame Especial                               |
+
+Metodologia:
+
+Estudos Dirigidos: atividades individuais práticas contendo exercícios e implementações dos métodos estudados. Serão avaliados com entrevistas ou testes durante as aulas.
+
+Leituras recomendadas: leitura de textos técnicos com a finalidade de proporcionar ao discente a oportunidade de consulta e desenvolvimento de sua capacidade de análise, síntese e crítica de uma bibliografia específica.
+
+Observações: A principal linguagem de programação deste curso será a linguagem Python. O código fonte dos trabalhos práticos será submetido pelo GitHub. O aluno precisará ter acesso à internet e um computador (desktop ou laptop).
+
+Atividades avaliativas:
+
+Estudos dirigidos (EDs) de 10 pontos.
+3 provas (P1, P2, P3) de 10 pontos
+Nota Final = (0.1 x P1 + 0.2 x P2 + 0.5 x P3 + 0.2 x média(ED))/10
+
+Exame Especial: Os alunos que tiverem pelo menos 75% de frequência (mínimo para aprovação) e
+média inferior a seis pontos poderão fazer o Exame Especial ou o Exame Especial Parcial. Estes
+exames serão provas únicas, individuais.
