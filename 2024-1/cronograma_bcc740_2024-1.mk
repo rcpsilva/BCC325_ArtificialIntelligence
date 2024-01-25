01d591f4fdb309c292f4c1c51efd5f461b41147f
rcpsilva <rcpsilva@gmail.com>
2024-01-25 15:02:34 -0300

    Create cronograma_bcc740_2024-1.mk

---
+++ 2024-1/cronograma_bcc740_2024-1.mk
@@ -0,0 +1,21 @@
+## Cronograma
+
+| Semana             | Conteúdo                                     |
+| ------------------ | -------------------------------------------- |
+| 25/03/2024 à 27/03/2024 | O que é Inteligência Artificial?         |
+| 01/04/2024 à 03/04/2024 | O que é Inteligência Artificial?         |
+| 08/04/2024 à 10/04/2024 | Busca em espaço de estados               |
+| 15/04/2024 à 17/04/2024 | Busca em espaço de estados               |
+| 22/04/2024 à 24/04/2024 | Problemas de statisfação de restrições   |
+| 29/04/2024              | Problemas de statisfação de restrições   |
+| 06/05/2024 à 08/05/2024 | Prova 1 (06/05) e Correção               |
+| 13/05/2024 à 15/05/2024 | Aprendizado de máquina                   |
+| 20/05/2024 à 22/05/2024 | Aprendizado de máquina                   |
+| 27/05/2024 à 29/05/2024 | Aprendizado de máquina                   |
+| 03/06/2024 à 05/06/2024 | Aprendizado de máquina                   |
+| 10/06/2024 à 12/06/2024 | Prova 2 (10/06) e Correção               |
+| 17/06/2024 à 19/06/2024 | Representação do conhecimento e inferência|
+| 24/06/2024 à 26/06/2024 | Representação do conhecimento e inferência|
+| 01/07/2024 à 03/07/2024 | Representação do conhecimento e inferência|
+| 10/07/2024 e 15/07/2024 | Prova 3 (10/07) e Correção                |
+| 22/07/2024              | Exame Especial                            |
