7e38c3bc28336f9ef38a09884f60346815c0d297
rcpsilva <rcpsilva@gmail.com>
2024-04-08 19:27:39 -0300

    updated simulation

--- 2024-1/SourceCode/Simulation.py
+++ 2024-1/SourceCode/Simulation.py
@@ -2,9 +2,9 @@ from Environment import Maze
 from Agent import RandomAgent
 
 start = [0,0]
-finish = [3,3]
-nrows = 4
-ncols = 4
+finish = [5,5]
+nrows = 6
+ncols = 6
 prob = 0.2
 
 maze = Maze(start,finish,nrows,ncols,prob,update_rate=0.05)
