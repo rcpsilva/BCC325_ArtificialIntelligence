29ef651605452e5c8315890713abb7c3f8baad8b
rcpsilva <rcpsilva@gmail.com>
2024-04-11 13:47:45 -0300

    added search agents

--- 2024-1/SourceCode/Agent.py
+++ 2024-1/SourceCode/Agent.py
@@ -30,8 +30,75 @@ class RandomAgent():
             for n in self.percepts['neighbors']:
                 self.F.append(path + [n])
 
+class DFSAgent():
 
+    def __init__(self,env):
 
+        self.percepts = env.initial_stimuli()
+        self.F = [[self.percepts['pos']]]
+        self.env = env
+
+    def act(self):
+        
+        while self.F:
+
+            path = self.F.pop(-1)
+
+            action = {'to_visit':path[-1],
+                      'path':path,
+                      'freeze':False}
+
+            self.percepts = self.env.react(action)
+
+            if all(path[-1] == self.percepts['finish']):
+                action = {'to_visit':path[-1],
+                      'path':path,
+                      'freeze':True}
+                self.percepts = self.env.react(action)
+                return path
+            
+            for n in self.percepts['neighbors']:
+                cycle = False
+                for node in path:
+                    if n[0]==node[0] and n[1]==node[1]:
+                        cycle = True
+                if not cycle:
+                    self.F.append(path + [n])
+                    
+class BFSAgent():
+
+    def __init__(self,env):
+
+        self.percepts = env.initial_stimuli()
+        self.F = [[self.percepts['pos']]]
+        self.env = env
+
+    def act(self):
+        
+        while self.F:
+
+            path = self.F.pop(0)
+
+            action = {'to_visit':path[-1],
+                      'path':path,
+                      'freeze':False}
+
+            self.percepts = self.env.react(action)
+
+            if all(path[-1] == self.percepts['finish']):
+                action = {'to_visit':path[-1],
+                      'path':path,
+                      'freeze':True}
+                self.percepts = self.env.react(action)
+                return path
+            
+            for n in self.percepts['neighbors']:
+                cycle = False
+                for node in path:
+                    if n[0]==node[0] and n[1]==node[1]:
+                        cycle = True
+                if not cycle:
+                    self.F.append(path + [n])
      
 
 
--- 2024-1/SourceCode/Simulation.py
+++ 2024-1/SourceCode/Simulation.py
@@ -1,14 +1,14 @@
 from Environment import Maze
-from Agent import RandomAgent
+from Agent import RandomAgent,DFSAgent,BFSAgent
 
 start = [0,0]
-finish = [5,5]
-nrows = 6
-ncols = 6
-prob = 0.2
+finish = [9,9]
+nrows = 10
+ncols = 10
+prob = 0.3
 
 maze = Maze(start,finish,nrows,ncols,prob,update_rate=0.05)
-ag = RandomAgent(maze)
+ag = BFSAgent(maze)
 
 ag.act()
 
