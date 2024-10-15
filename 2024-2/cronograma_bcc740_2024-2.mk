7db4232de1cee6807b0e4dc3df2537e7d79d4560
rcpsilva <rcpsilva@gmail.com>
2024-10-14 21:09:46 -0300

    Create cronograma_bcc740_2024-2.mk

---
+++ 2024-2/cronograma_bcc740_2024-2.mk
@@ -0,0 +1,40 @@
+## Cronograma
+
+Semanas                 & Conteúdo 
+----------------------- & ------------- 
+1 & Introdução
+2 & Sem aulas (Participação do Brazilian Conference on Intelligent Systems (BRACIS 2024))
+3 e 4 & Busca
+5 e 6 & Representação e Manipulação do Conhecimento (Entrega Projeto 1)           
+7 e 8 & Incerteza (Entrega Projeto 2)
+9 e 10 & Otimização  (Entrega Projeto 3)
+11 e 12 & Aprendizado  (Entrega Projeto 4)
+13 e 14 & Redes Neurais (Entrega Projeto 5)
+15 e 16 & Linguagem (Entrega Projeto 6)
+17 & Apreciação dos Trabalhos (Entrega Projeto 7)
+07/04/2025 ------------ & Exame Especial 
+
+Metodologia:
+
+Projetos práticos: Implementação de vários algoritmos e técnicas de inteligência artificial. Podem ser avaliados por meio de apresentações e entrevistas.
+
+Quizzes: Conjunto de perguntas para medir o conhecimento teórico do aluno durante as aulas. Realizado sob demanda do professor.
+
+Leituras recomendadas: Leitura de textos técnicos com a finalidade de proporcionar ao discente a oportunidade de consulta e desenvolvimento de sua capacidade de análise, síntese e crítica de uma bibliografia específica.
+
+Observações: A principal linguagem de programação deste curso será a linguagem Python. O código fonte dos trabalhos práticos será submetido pelo GitHub. O aluno precisará ter acesso à internet e um computador (desktop ou laptop).
+
+Atividades avaliativas:
+
+7 Projetos Práticos (P1, P2, P3, P4, P5, P6, P7) de 10 pontos
+7 Quizzes (Q1, Q2, Q3, Q4, Q5, Q6, Q7) de 10 pontos
+
+Nota Final = (	0.05 x P1 + 0.01 x Q1 +
+				0.05 x P2 + 0.01 x Q2 +
+				0.05 x P3 + 0.02 x Q3 +
+				0.05 x P4 + 0.02 x Q4 +
+				0.05 x P5 + 0.03 x Q5 +
+				0.05 x P6 + 0.03 x Q6 +
+				0.30 x P7 + 0.28 x Q7 )
+
+Exame Especial: Os alunos que tiverem pelo menos 75% de frequência (mínimo para aprovação) e média inferior a seis pontos poderão fazer o Exame Especial ou o Exame Especial Parcial. Estes exames serão provas únicas, individuais.
