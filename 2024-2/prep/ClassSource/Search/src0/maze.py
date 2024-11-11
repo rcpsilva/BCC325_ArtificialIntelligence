8875a4c30032db2d167089741562e2da22533e30
rcpsilva <rcpsilva@gmail.com>
2024-11-11 20:09:24 -0300

    uploaded plano de ensino

--- 2024-2/prep/ClassSource/Search/src0/maze.py
+++ 2024-2/prep/ClassSource/Search/src0/maze.py
@@ -124,7 +124,7 @@ class Maze():
 
         # Initialize frontier to just the starting position
         start = Node(state=self.start, parent=None, action=None)
-        frontier = StackFrontier()
+        frontier = QueueFrontier()
         frontier.add(start)
 
         # Initialize an empty explored set
