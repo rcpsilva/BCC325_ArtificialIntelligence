1faaa327da297fa611dc459d4fad372d73d2aa6b
rcpsilva <rcpsilva@gmail.com>
2025-10-21 16:46:01 -0300

    fixed search agents

--- 2025-2/LectureScripts/Script3_HeuristicSearch/code/agents.py
+++ 2025-2/LectureScripts/Script3_HeuristicSearch/code/agents.py
@@ -1,17 +1,23 @@
 import numpy as np
+from heapq import heappush, heappop
+from itertools import count
 
 class Node:
 
-    def __init__(self, state, parent=None, g=0.0):
+    def __init__(self, state, parent=None, g=0.0, h=0.0):
         self.state  = state
         self.parent = parent
         self.g      = g
+        self.h      = h
         self.depth  = 0 if parent is None else parent.depth + 1
 
     def __eq__(self, other):
         if not isinstance(other, Node):
             return NotImplemented
         return np.array_equal(self.state, other.state)
+    
+    def __lt__(self, other):
+        return True
 
     def __hash__(self):
         return hash(self.state.tobytes())
@@ -25,74 +31,65 @@ def get_solution(node):
     solution.reverse()
     return solution
 
-class AgentDFSMaze:
 
-    def __init__(self, env):
+def remove_last(F):
+    return F.pop(-1)
+
+def add_last(F,s):
+    F.append(s)
+
+def remove_first(F):
+    return F.pop(0)
+
+def cost_manhattan(parent_node):
+    return parent_node.g + 1
+
+def h_manhattan(state,exit):
+    return np.sum(np.abs(state - exit))
+
+def add_heap_astar(F,s):
+    heappush(F, (s.h+s.g, s))
+
+def add_heap_greedy(F,s):
+    heappush(F, (s.h, s))
+
+def remove_heap(F):
+    return heappop(F)[1]
+
+class AgentMaze:
+
+    def __init__(self, env, add_fcn, remove_fcn, cost_fcn, h_fcn):
         self.env = env
         self.visited = set()
         self.initial_percepts = env.initial_percepts()
         self.G = self.initial_percepts['exit']
+        self.remove_fcn = remove_fcn
+        self.add_fcn = add_fcn
+        self.h_fcn = h_fcn
+        self.cost_fcn = cost_fcn
+
 
     def search(self):
 
-        s0 = Node(self.initial_percepts['start'])
-
-        if (self.initial_percepts['start'] == self.G).all():
-            return s0 
+        s0 = Node(self.initial_percepts['start'], g=self.cost_fcn(Node(self.initial_percepts['exit'])), h=self.h_fcn(self.initial_percepts['exit'],self.G))
         
-        self.visited.add(s0)
-        F = [s0]
+        F = []
+
+        self.add_fcn(F,s0)
 
         while F:
 
-            s = F.pop(-1)
-            
+            s = self.remove_fcn(F)
             print(s.state)
+
+            if (s.state == self.G).all():
+                return s
             
+            self.visited.add(s)
+                        
             for s_ in self.env.get_neighbors(s.state):
-                s_node = Node(s_,s)
+                s_node = Node(s_,s,self.cost_fcn(s),self.h_fcn(s_,self.G))
                 if s_node not in self.visited:
-                    if (s_node.state == self.G).all():
-                        return s_node
-                    self.visited.add(s_node)
-                    F.append(s_node)
+                    self.add_fcn(F,s_node)
 
         return None
-    
-class AgentBFSMaze:
-
-    def __init__(self, env):
-        self.env = env
-        self.visited = set()
-        self.initial_percepts = env.initial_percepts()
-        self.G = self.initial_percepts['exit']
-
-    def search(self):
-
-        s0 = Node(self.initial_percepts['start'])
-
-        if (self.initial_percepts['start'] == self.G).all():
-            return s0 
-        
-        self.visited.add(s0)
-        F = [s0]
-
-        while F:
-
-            s = F.pop(0)
-            
-            print(s.state)
-            
-            for s_ in self.env.get_neighbors(s.state):
-                s_node = Node(s_,s)
-                if s_node not in self.visited:
-                    if (s_node.state == self.G).all():
-                        return s_node
-                    self.visited.add(s_node)
-                    F.append(s_node)
-
-        return None
-
-
-
-
--- 2025-2/LectureScripts/Script3_HeuristicSearch/code/simulation.py
+++ 2025-2/LectureScripts/Script3_HeuristicSearch/code/simulation.py
@@ -1,29 +1,50 @@
-from agents import AgentDFSMaze, AgentBFSMaze, get_solution
+from agents import *
 from environments import Maze
 
 matrix = [[0,0,0,0,0,0,0,0],
+        [0,1,0,0,1,1,1,1],
+        [0,1,0,0,0,0,0,0],
+        [0,1,0,0,0,0,0,0],
+        [0,1,0,1,1,1,1,0],
+        [0,1,0,0,0,0,0,0],
         [0,1,0,0,0,0,0,1],
-        [0,1,0,0,0,0,0,0],
-        [0,1,0,0,0,0,0,0],
-        [0,1,0,0,0,0,0,0],
-        [0,1,0,0,0,0,0,0],
-        [0,1,0,0,0,0,0,0],
         [0,1,0,0,0,0,0,0]]
 
+'''
 matrix = [[0,0,0,0],
           [1,0,1,0],
           [0,0,0,0],
           [0,1,0,1],
           [0,0,0,0]]
 
+matrix = [[0,0,0,0],
+          [0,0,0,0],
+          [0,0,0,0],
+          [0,0,0,0],
+          [0,0,0,0],
+          [0,0,0,0]]
+'''
 
 maze = Maze(matrix,[0,0],[len(matrix)-1,len(matrix[0])-1])
-agent = AgentBFSMaze(maze)
 
-res = agent.search()
+dfs = AgentMaze(maze, add_last, remove_last, cost_manhattan, h_manhattan)
+bfs = AgentMaze(maze, add_last, remove_first, cost_manhattan, h_manhattan)
+greedy= AgentMaze(maze, add_heap_greedy, remove_heap, cost_manhattan, h_manhattan)
+astar = AgentMaze(maze, add_heap_astar, remove_heap, cost_manhattan, h_manhattan)
 
-print(res.state)
+res_dfs = dfs.search()
+res_bfs = bfs.search()
+res_greedy = greedy.search()
+res_astar = astar.search()
 
-print(get_solution(res))
 
-maze.plot(get_solution(res))
+print(f'dfs cost:\t {res_dfs.g}')
+print(f'bfs cost:\t {res_bfs.g}')
+print(f'greedy cost:\t {res_greedy.g}')
+print(f'astar cost:\t {res_astar.g}')
+
+
+maze.plot(get_solution(res_dfs))
+maze.plot(get_solution(res_bfs))
+maze.plot(get_solution(res_greedy))
+maze.plot(get_solution(res_astar))
